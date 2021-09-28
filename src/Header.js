3c9af242bfcd4db58fb2b3de19a33c87895b3336
SaifNavid <saif.navids8@gmail.com>
2021-09-28 16:41:30 +0100

    initial header work

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Header.css"
+import SearchIcon from '@mui/icons-material/Search';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>This is the header</h1>
+
+            <div className="header__left">
+                <img src="./Images/linkedin.svg" alt="hello"/>
+                
+
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+
+            <div className="header__right">
+
+            </div>
+        </div>
+    )
+}
+
+export default Header
